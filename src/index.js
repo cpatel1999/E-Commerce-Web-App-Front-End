b4b60158c247051e9d8f1323df54277be6f65230
Charit_Patel <charit15499@gmail.com>
2023-02-22 00:47:04 -0500

    Routing pages using react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import Routes from './Routes';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Routes />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(<Routes />, document.getElementById("root"))
